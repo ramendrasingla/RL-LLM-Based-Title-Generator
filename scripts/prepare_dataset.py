11a3cad6c7682d19e6097deab783c88c885b674a
Ramendra Singla <ramendrasingla@gmail.com>
2024-10-23 15:53:18 +0530

    Data Ingestion and preprocessing code base added

--- scripts/prepare_dataset.py
+++ scripts/prepare_dataset.py
@@ -1,99 +1,61 @@
-import requests
-import pandas as pd
+import sys
 import os
-from dotenv import load_dotenv
-from datetime import datetime
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
 
-# Load environment variables from the .env file
-load_dotenv(".env")
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
-# Your News API key
-API_KEY = os.getenv('API_KEY')
+from utils.data_ingestion import data_ingestion_pipeline
+from utils.data_preprocessing import preprocess_text
+from utils.data_storage import save_dataframe
+from utils.helper_funcs import setup_logging
+from utils.constants import DATA_INGESTION_OUTPUT_FILE, PREPROCESSED_DATA_PATH
 
-BASE_PATH = './data/raw'
+# Setup Logging
+global logger
+logger = setup_logging()
 
-# Function to fetch articles
-def fetch_articles(from_date, to_date, page=1):
-    url = (f'https://newsapi.org/v2/everything?'
-           'domains=medium.com,techcrunch.com&'
-           f'from={from_date}&'  # Start date
-           f'to={to_date}&'      # End date
-           f'page={page}&'       # Pagination
-           'language=en&'
-           'apiKey=' + API_KEY)
+def prepare_dataset(load_type = 'init', run_data_ingest = True, splits = [0.2, 0.25]):
 
-    response = requests.get(url)
-    data = response.json()
+    if load_type == 'init' and run_data_ingest:
+        # Data Ingestion
+        data_ingestion_pipeline(logger=logger, is_init_load=True)
+            
+    elif run_data_ingest:
+        # Data Ingestion
+        data_ingestion_pipeline(logger=logger, is_init_load=False)
     
-    if data.get('status') == 'ok':
-        return data['articles']
+    # Data Preprocessing
+    df = pd.read_json(DATA_INGESTION_OUTPUT_FILE)
+    df['title'] = df['title'].apply(preprocess_text)
+    df['content'] = df['content'].apply(preprocess_text)
+    preprocessed_df = df[['title', 'content', 'theme']].drop_duplicates()
+
+    # Train, Test Split
+    train_val_df, test_df = train_test_split(preprocessed_df, test_size=splits[0], stratify=preprocessed_df['theme'], random_state=42)
+    # Train, Validation Split
+    train_df, val_df = train_test_split(train_val_df, test_size=splits[1], stratify=train_val_df['theme'], random_state=42)
+
+    # Saving Processed dataframes
+    if load_type == 'init':
+        save_type = "overwrite"
     else:
-        print("Error fetching data:", data.get('message'))
-        return []
+        save_type = "append"
 
-# Function to save articles to CSV
-def save_to_csv(articles, month_year):
-    df = pd.DataFrame(articles)
-    
-    # Create directory if it doesn't exist
-    if not os.path.exists(BASE_PATH):
-        os.makedirs(BASE_PATH)
-    
-    # Save to CSV
-    csv_file_path = f'{BASE_PATH}/articles_{month_year}.csv'
-    df.to_csv(csv_file_path, index=False, mode='a', header=not os.path.isfile(csv_file_path))
-    print(f'Saved articles to {csv_file_path}')
+    save_dataframe(logger, train_df, PREPROCESSED_DATA_PATH / "train.json", save_type)
+    save_dataframe(logger, test_df, PREPROCESSED_DATA_PATH / "test.json", save_type)
+    save_dataframe(logger, val_df, PREPROCESSED_DATA_PATH / "val.json", save_type)
 
-# Initialize the last processed date
-last_processed_date = None
 
-# Load the last processed date from a file if it exists
-if os.path.exists('last_processed_date.txt'):
-    with open('last_processed_date.txt', 'r') as f:
-        last_processed_date = datetime.strptime(f.read().strip(), '%Y-%m-%d')
+if __name__ == "__main__":
 
-# Start from a default date if no last processed date exists
-if last_processed_date is None:
-    last_processed_date = datetime(2017, 1, 1)
 
-# Loop through months until the present
-end_date = datetime.now()
-
-while last_processed_date < end_date:
-    # Define the date range for the current month
-    start_of_month = last_processed_date.replace(day=1)
-    if last_processed_date.month == 12:
-        end_of_month = start_of_month.replace(year=start_of_month.year + 1, month=1)
+    # Ask the user if they want to run the initial load or delta load
+    load_type = input("What kind of load type do you want to run(init/delta)? ").strip().lower()
+    logger.info(f"Running {load_type} load...")
+    if load_type == 'delta':
+        # In case of delta load, ask if we want to run data ingestion or not
+        run_data_ingest = input("Do we want to run data ingestion(y/n)? ").strip().lower()
+        prepare_dataset(load_type= load_type, run_data_ingest= (run_data_ingest == 'y'))
     else:
-        end_of_month = start_of_month.replace(month=start_of_month.month + 1)
-
-    # Format dates for API
-    from_date = start_of_month.strftime('%Y-%m-%d')
-    to_date = end_of_month.strftime('%Y-%m-%d')
-
-    # Fetch articles for the current month
-    page = 1
-    all_articles = []
-
-    while True:
-        articles = fetch_articles(from_date, to_date, page)
-        
-        if not articles:
-            break  # Stop if no articles are returned
-
-        all_articles.extend(articles)
-        page += 1
-
-    # Partition articles by month-year
-    if all_articles:
-        month_year = start_of_month.strftime('%Y-%m')
-        save_to_csv(all_articles, month_year)
-        
-        # Update last processed date to the end of the current month
-        last_processed_date = end_of_month
-
-# Save the last processed date
-with open('last_processed_date.txt', 'w') as f:
-    f.write(last_processed_date.strftime('%Y-%m-%d'))
-
-print("Article fetching and saving completed.")
+        prepare_dataset(load_type= load_type, run_data_ingest= True)
